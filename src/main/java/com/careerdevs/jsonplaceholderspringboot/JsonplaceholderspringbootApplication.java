fc24a1fb94ad3897be7f48b0959e4a9f2a007bc3
“JazlynL” <lubenjazlyn35@gmail.com>
2022-03-01 20:27:15 -0500

    Initial commit

---
+++ src/main/java/com/careerdevs/jsonplaceholderspringboot/JsonplaceholderspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.careerdevs.jsonplaceholderspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonplaceholderspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonplaceholderspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/careerdevs/jsonplaceholderspringboot/JsonplaceholderspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.jsonplaceholderspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonplaceholderspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
